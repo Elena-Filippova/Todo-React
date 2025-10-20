97e57b7f0b6e470a45108c1d8e1f49b84780d1fe
Elena-Filippova <157601139+Elena-Filippova@users.noreply.github.com>
2025-10-20 14:23:42 +0300

    Add files via upload

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,13 +1,63 @@
+import { useState } from 'react'
+import { ToDo } from '../assets/models/todo-item'
 import { Form } from '../components/form/Form'
 import { Header } from '../components/header/Header'
 import { ToDoList } from '../components/toDoList/ToDoList'
+import { ToastContainer, Zoom, toast } from 'react-toastify'
 
 export const ToDoListPage = () => {
+  const [todos, setTodos] = useState<ToDo[]>([])
+
+  const notifyCreate = () => toast('Create')
+  const notifyUpdate = () => toast('Update')
+  const notifyDelete = () => toast('Delete')
+
+  const createNewToDo = (text: string) => {
+    const newToDo: ToDo = {
+      id: todos.length,
+      text: text,
+      isDone: false
+    }
+    notifyCreate()
+    setTodos([...todos, newToDo])
+  }
+
+  const updateToDo = (toDoItem: ToDo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === toDoItem.id) {
+        todo.isDone = !todo.isDone
+      }
+
+      return todo
+    })
+    notifyUpdate()
+    setTodos(newTodos)
+  }
+
+  const deleteToDo = (toDoItem: ToDo) => {
+    const newTodos = todos.filter((todo) => todo.id !== toDoItem.id)
+    notifyDelete()
+    setTodos(newTodos)
+  }
+
   return (
     <>
       <Header />
-      <Form />
-      <ToDoList />
+      <Form createNewToDo={createNewToDo} />
+      <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
+      <ToastContainer 
+        position='bottom-right'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Zoom}
+      />
     </>
   )
 }
